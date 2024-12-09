c5e8f258ec5654c98adc174e5133e6ac6e883d8c
belalhamdani <belal_hamdani26@milton.edu>
2024-12-09 09:06:01 -0500

    initial

---
+++ src/Array1D.java
@@ -0,0 +1,41 @@
+public class Array1D {
+    int[] numbers = new int[10];
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Array1D a1d = new Array1D();
+    }
+
+
+    public Array1D() {
+        numbers[0] = 54;
+        for (int i = 0; i < numbers.length; i++) {
+            int tim = (int) (Math.random() * 101);
+            numbers[i] = tim;
+        }
+        printArray();
+        System.out.println("the sum is " +sumArray());
+    }
+
+    public void printArray() {
+        //write code to print numbers array
+        for (int x = 0; x < numbers.length; x++) {
+            System.out.println(numbers[x]);
+        }
+    }
+
+    public int sumArray(){
+        int sum = 0;
+        //write code for sum
+        for(int w = 0; w < numbers.length; w++) {
+            sum += numbers[w];
+        }
+
+
+
+        return sum;
+    }
+    public double avgArray(){
+
+    }
+}
